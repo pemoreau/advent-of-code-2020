1bdedfcde1fad5cecd962609aa73a749bae9d4dc
pem <pierre-etienne.moreau@loria.fr>
2021-01-10 19:11:37 +0100

    day03 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -1,12 +1,9 @@
 // use std::env::var;
 
 pub fn part1(input: String) {
-    let values: Vec<_> = input
-        .lines()
-        .map(|line| line.chars().as_str().repeat(100))
-        .collect();
-    let cpt = values.iter().enumerate().fold(0, |acc, (i, line)| {
-        if line.chars().nth(3 * i).unwrap() == '#' {
+    let values = input.lines().map(|line| line.chars().cycle());
+    let cpt = values.enumerate().fold(0, |acc, (i, line)| {
+        if line.clone().nth(3 * i).unwrap() == '#' {
             acc + 1
         } else {
             acc
@@ -15,4 +12,24 @@ pub fn part1(input: String) {
     println!("cpt={}", cpt);
 }
 
-pub fn part2(input: String) {}
+pub fn part2(input: String) {
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let cpt = slopes.iter().fold(1, |prod, (x, y)| {
+        let values = input.lines().map(|line| line.chars().cycle());
+        let cpt = values.enumerate().fold(0, |acc, (j, line)| {
+            if j % y == 0 {
+                let i = (x * j) / y;
+                let element = line.clone().nth(i).unwrap();
+                if element == '#' {
+                    acc + 1
+                } else {
+                    acc
+                }
+            } else {
+                acc
+            }
+        });
+        prod * cpt
+    });
+    println!("cpt={}", cpt);
+}
